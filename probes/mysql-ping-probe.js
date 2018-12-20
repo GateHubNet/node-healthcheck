b6e7c3a8ed386632928142a18a35559c09a249ae
Matej Cotman <matej@gatehub.net>
2018-12-20 17:11:07 +0100

    fix(mysql-ping-probe): do not resolve on error

--- probes/mysql-ping-probe.js
+++ probes/mysql-ping-probe.js
@@ -1,32 +1,40 @@
-const wrap = require('../function-wrapper.js')
-	  PingProbe = require('./ping-probe.js');
+const wrap = require('../function-wrapper.js');
+const PingProbe = require('./ping-probe.js');
 
 module.exports = class MysqlProbe extends PingProbe {
-	constructor() {
-		super('mysql');
+    constructor() {
+        super('mysql');
 
-		this.client = null;
-		this.attached = false;
-	}
+        this.client = null;
+        this.attached = false;
+    }
 
-	// Attach should extract the mysql object
-	attach(target) {
-		if(!target || target.__healthProbeAttached__) return target;
-		target.__healthProbeAttached__ = this.attached = true;
+    // Attach should extract the mysql object
+    attach(target) {
+        if (!target || target.__healthProbeAttached__) return target;
+        target.__healthProbeAttached__ = this.attached = true;
 
-		let data = {};
-		wrap.after(target, 'createConnection', data, (target, methodName, args, probeData, ret) => {
-			this.client = ret;
-			return ret;
-		});
+        let data = {};
+        wrap.after(target, 'createConnection', data, (target, methodName, args, probeData, ret) => {
+            this.client = ret;
+            return ret;
+        });
 
-		return target;
-	}
+        return target;
+    }
 
-	ping(timeout) {
-		return new Promise((res, rej) => {
-			if(!this.client) return rej();
-			this.client.ping({ timeout }, (err) => err && res() || rej());
-		});
-	}
-}
+    ping(timeout) {
+        return new Promise((res, rej) => {
+            if (!this.client) return rej();
+            this.client.ping({
+                timeout
+            }, (err) => {
+                if (err) {
+                    rej();
+                } else {
+                    res();
+                }
+            });
+        });
+    }
+};
